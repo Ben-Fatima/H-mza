265f88e2ebf48685722f0f92395f7b2edd559c37
Ben-Fatima <fatimazahranit@gmail.com>
2021-12-21 20:04:34 +0100

    create migrations

---
+++ database/migrations/2021_12_15_200958_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id');
+            $table->string('name');
+            $table->integer('life_time');
+            $table->float('price');
+            $table->float('weight');
+            $table->float('width');
+            $table->float('height');
+            $table->float('depth');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
--- database/migrations/2021_12_21_185641_create_offers_table.php
+++ database/migrations/2021_12_21_185641_create_offers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateOffersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('offers', function (Blueprint $table) {
             $table->id();
-            $table->string('user_name');
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignId('product_id');
+            $table->foreignId('account_id');
+            $table->string('title');
+            $table->timestamp('date');
+            $table->string('description');
+            $table->string('adress');
             $table->timestamps();
         });
     }
@@ -29,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('offers');
     }
 }
